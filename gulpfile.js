822782f4ff2c6944e271dc34d635029bfa1db22a
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-09-20 22:43:59 +0200

    run build version with minimalize css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ let gulp = require('gulp');
 let browserSync = require('browser-sync');
 let sass = require('gulp-sass');
 let useref = require('gulp-useref');
+var runSequence = require('run-sequence').use(gulp);
+var del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
 
 gulp.task('browserSync', function() {
     browserSync.init({
@@ -12,20 +17,24 @@ gulp.task('browserSync', function() {
 });
 
 gulp.task('sass', function () {
-    return gulp.src('./src/sass/**/*.scss') // gwiazdki - każdy plik scss, nawet plik - dziecko - zagnieżdżony
+    return gulp.src('./src/sass/**/*.scss') 
       .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer('last 2 version'))
       .pipe(gulp.dest('./src/css'))
       .pipe(browserSync.reload({
           stream:true
       }))
   });
 
-  gulp.task('watch', ['browserSync', 'sass'], ()=> { // nawias kwadratowy - taski które mają sie wykonac przed uruch. watch
+  gulp.task('watch', ['browserSync', 'sass'], ()=> { 
     gulp.watch('src/sass/**/*.scss', ['sass'])
     gulp.watch('src/*.html', browserSync.reload)
     gulp.watch('src/js/**/*.js', browserSync.reload)
   })
 
+  gulp.task('clean:dist', () => {
+    return del.sync('dist');
+  })
 
 gulp.task('useref', ()=> {
     return gulp.src('src/*.html')
@@ -33,3 +42,14 @@ gulp.task('useref', ()=> {
         .pipe(gulpIf('*.css', cssnano()))
         .pipe(gulp.dest('dist'))
 })
+
+gulp.task('serve', function (callback) {
+    runSequence(['watch', 'sass', 'browserSync'],
+      callback
+    )
+  })
+
+  gulp.task('build', function (callback) {
+    runSequence('clean:dist', ['serve'], 'useref',
+      callback)
+ })
